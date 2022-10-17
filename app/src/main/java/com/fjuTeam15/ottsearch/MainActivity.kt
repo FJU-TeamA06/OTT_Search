b57cee789d89af2ef421df42715fdbffa322d7f4
andyching168 <andyching168@gmail.com>
2022-10-17 12:33:33 +0800

    搜尋介面創建

--- app/src/main/java/com/fjuTeam15/ottsearch/MainActivity.kt
+++ app/src/main/java/com/fjuTeam15/ottsearch/MainActivity.kt
@@ -2,10 +2,16 @@ package com.fjuTeam15.ottsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+fun swac(view: View){
+    val intent = Intent(this, mc2::class.java )
+    startActivity(intent)
 }
