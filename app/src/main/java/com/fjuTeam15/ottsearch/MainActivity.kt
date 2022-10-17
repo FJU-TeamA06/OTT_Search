5488387cf1ca729e040e811d70405e37b8888929
andyching168 <andyching168@gmail.com>
2022-10-17 12:51:06 +0800

    搜尋介面連接

--- app/src/main/java/com/fjuTeam15/ottsearch/MainActivity.kt
+++ app/src/main/java/com/fjuTeam15/ottsearch/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var buttonSearch: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonSearch = findViewById(R.id.buttonSearch)
     }
-}
-fun swac(view: View){
-    val intent = Intent(this, mc2::class.java )
-    startActivity(intent)
+    fun SwitchToSearch(view: View){
+        val intent = Intent(this, SearchActivity::class.java )
+        startActivity(intent)
+    }
 }
