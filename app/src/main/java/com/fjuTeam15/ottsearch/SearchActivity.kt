c1d5504775f77c5ba7835d7b58346d1a67d7ae16
andyching168 <andyching168@gmail.com>
2022-10-21 22:10:02 +0800

    把Spinner(下拉目錄新增項目)

--- app/src/main/java/com/fjuTeam15/ottsearch/SearchActivity.kt
+++ app/src/main/java/com/fjuTeam15/ottsearch/SearchActivity.kt
@@ -1,12 +1,19 @@
 package com.fjuTeam15.ottsearch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SearchActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         title = "查詢"
+        val spinner1 = findViewById<Spinner>(R.id.spinnerOTTs)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.OTTs, android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = adapter
     }
 }
