e8a43307cc17ece1677b701002b4b82fa20fb02b
SKMOTT <webber1218@gmail.com>
2022-11-11 14:30:28 +0800

    熱門 新增 Spinner

--- app/src/main/java/com/fjuTeam15/ottsearch/HitActivity.kt
+++ app/src/main/java/com/fjuTeam15/ottsearch/HitActivity.kt
@@ -2,11 +2,16 @@ package com.fjuTeam15.ottsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class HitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hit)
         title = "熱門"
+        val spinner1 = findViewById<Spinner>(R.id.spinnerHIT)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.HIT, android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = adapter
     }
 }
